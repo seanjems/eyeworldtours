e98e320b36cff2dc9ca35b3ba9f00d728fce9dca
Najuna James <najuna@amarinfinancial.com>
2022-10-23 21:06:32 +0300

    cleanedup routes

--- src/main-component/router/index.js
+++ src/main-component/router/index.js
@@ -41,23 +41,23 @@ const AllRoute = () => {
           <Route path="about" element={<AboutPage />} />
           {/* <Route path="search-result" element={<SearchResults />} /> */}
           {/* <Route path="room-single/:id" element={<RoomSinglePage />} /> */}
-          <Route path="cart" element={<CartPage />} />
-          <Route path="checkout" element={<CheckoutPage />} />
-          <Route path="order_received" element={<OrderRecived />} />
-          <Route path="room" element={<RoomPage />} />
+          {/* <Route path="cart" element={<CartPage />} /> */}
+          {/* <Route path="checkout" element={<CheckoutPage />} /> */}
+          {/* <Route path="order_received" element={<OrderRecived />} /> */}
+          {/* <Route path="room" element={<RoomPage />} /> */}
           <Route path="destination" element={<DestinationPage />} />
           <Route
             path="destination-single/:id"
             element={<DestinationSinglePage />}
           />
           <Route path="places/:id" element={<PlacesSinglePage />} />
-          <Route path="service" element={<ServicePage />} />
+          {/* <Route path="service" element={<ServicePage />} />
           <Route path="service-s2" element={<ServicePageS2 />} />
-          <Route path="service-single/:id" element={<ServiceSinglePage />} />
+          <Route path="service-single/:id" element={<ServiceSinglePage />} /> */}
           <Route path="places" element={<PricingPage />} />
-          <Route path="404" element={<ErrorPage />} />
+          {/* <Route path="404" element={<ErrorPage />} /> */}
           <Route path="contact" element={<ContactPage />} />
-          <Route path="blog-single/:id" element={<BlogDetails />} />
+          {/* <Route path="blog-single/:id" element={<BlogDetails />} />
           <Route
             path="blog-single-left-sidebar/:id"
             element={<BlogDetailsLeftSiide />}
@@ -71,7 +71,7 @@ const AllRoute = () => {
           <Route path="blog-fullwidth" element={<BlogPageFullwidth />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<SignUpPage />} />
-          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="forgot-password" element={<ForgotPassword />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
